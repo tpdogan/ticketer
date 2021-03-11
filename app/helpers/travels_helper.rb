7086341d8d2f823a810a9796f945b965b6abbbf8
PandaPera <pandapera2020@gmail.com>
2021-03-11 16:41:22 +0100

    Helper method to find distance between 2 cities

--- app/helpers/travels_helper.rb
+++ app/helpers/travels_helper.rb
@@ -1,2 +1,17 @@
 module TravelsHelper
+  def distance(city_start, city_finish)
+    lat1 = city_start.latitude
+    lon1 = city_start.longitude
+    lat2 = city_finish.latitude
+    lon2 = city_finish.longitude
+    R = 6378.137
+    dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180
+    dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180
+    a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+            Math.sin(dLon/2) * Math.sin(dLon/2)
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
+    d = R * c
+    return d;
+  end
 end
