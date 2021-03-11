e16bfa559a43104f92abc099b1eea540f52651a9
PandaPera <pandapera2020@gmail.com>
2021-03-11 18:06:11 +0100

    Travel helper distance bug is fixed

--- app/helpers/travels_helper.rb
+++ app/helpers/travels_helper.rb
@@ -4,14 +4,14 @@ module TravelsHelper
     lon1 = city_start.longitude
     lat2 = city_finish.latitude
     lon2 = city_finish.longitude
-    R = 6378.137
+    r = 6378.137
     dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180
     dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180
     a = Math.sin(dLat/2) * Math.sin(dLat/2) +
             Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
             Math.sin(dLon/2) * Math.sin(dLon/2)
     c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a))
-    d = R * c
+    d = r * c
     return d;
   end
 end
