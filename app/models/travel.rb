dd7e418aa5a2d090996d781be92aacfe2ad4528f
PandaPera <pandapera2020@gmail.com>
2021-03-11 09:23:42 +0100

    passenger model is created and associated with travels

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -1,4 +1,6 @@
 class Travel < ApplicationRecord
   belongs_to :start, class_name: 'City'
   belongs_to :finish, class_name: 'City'
+
+  has_many :passengers
 end
---
+++ db/migrate/20210311081626_create_passengers.rb
@@ -0,0 +1,22 @@
+class CreatePassengers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :passengers do |t|
+      # A passenger will have a single travel
+      # Many transfers will be saved as different travels
+      t.integer :travel_id
+      
+      t.string  :first_name
+      t.string  :last_name
+
+      t.string :date_of_birth
+      t.string :passport_number
+      t.string :nationality
+
+      # Address could well be extended
+      t.string :address
+      t.string :tel
+
+      t.timestamps
+    end
+  end
+end
