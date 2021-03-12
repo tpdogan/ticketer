fbaa07f1010a673f01af324ebdd31dee25ceb1cf
PandaPera <pandapera2020@gmail.com>
2021-03-12 11:55:37 +0100

    Fetching is modulized in JS

--- app/javascript/packs/form.js
+++ app/javascript/packs/form.js
@@ -1,4 +1,5 @@
 import autocomplete from './modules/autocomplete'
+import fetchPaths from './modules/fetchPaths'
 import transferCheck from './modules/transfer'
 
 const cities = JSON.parse(document.getElementById('city_list').value)
@@ -24,32 +25,5 @@ function fillSelect(target) {
   const id = target.id
   const parentId = target.parentElement.parentElement.id.substr(15)
   document.getElementById(parentId).value = id
-  fetchSelect()
-}
-
-function fetchSelect() {
-  const from = document.getElementById('start').value
-  const to = document.getElementById('finish').value
-  if (cityArray.includes(from) && cityArray.includes(to)) {
-    console.log(`Travel from ${from} to ${to}!`)
-
-    const url ='http://localhost:3000/travels'
-    const adults = document.getElementById('passenger_count').value
-    const children = document.getElementById('child_count').value
-    const transfer = document.getElementById('transfer').value
-    const requestURL = url + `?request=path&from=${from}&to=${to}&passengers=${adults + children}&transfer=${transfer}`
-
-    fetch(requestURL, {
-      cache: 'no-cache',
-      headers: {
-        'Accept': 'application/json'
-      }
-    })
-      .then((response) => {
-        return response.text()
-      })
-        .then((data) => {
-          console.log(data)
-        })
-  }
+  fetchPaths(cityArray)
 }
---
+++ app/javascript/packs/modules/fetchPaths.js
@@ -0,0 +1,29 @@
+function fetchPaths(cityArray) {
+  const from = document.getElementById('start').value
+  const to = document.getElementById('finish').value
+  if (cityArray.includes(from) && cityArray.includes(to)) {
+    console.log(`Travel from ${from} to ${to}!`)
+
+    const url ='http://localhost:3000/travels'
+    const adults = document.getElementById('passenger_count').value
+    const children = document.getElementById('child_count').value
+    const transfer = document.getElementById('transfer').value
+    const requestURL = url + `?request=path&from=${from}&to=${to}&passengers=${adults + children}&transfer=${transfer}`
+
+    fetch(requestURL, {
+      cache: 'no-cache',
+      headers: {
+        'Accept': 'application/json'
+      }
+    })
+      .then((response) => {
+        return response.text()
+      })
+        .then((data) => {
+          console.log(data)
+        })
+  }
+
+}
+
+export default fetchPaths
