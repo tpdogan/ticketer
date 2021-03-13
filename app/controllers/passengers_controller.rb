d9c6dbbe7a73387695b092b4078033d380ece25c
PandaPera <pandapera2020@gmail.com>
2021-03-14 00:26:16 +0100

    Passengers are created successfully

--- app/controllers/passengers_controller.rb
+++ app/controllers/passengers_controller.rb
@@ -1,4 +1,6 @@
 class PassengersController < ApplicationController
+  def index
+  end
   def new
     @passengers = []
     params[:count].to_i.times do |i|
@@ -6,8 +8,30 @@ class PassengersController < ApplicationController
     end
   end
   def create
+    @travels = [Travel.find_by_no(travel_params[:no_1])]
+    travel_params[:no_2] ? @travels.push(Travel.find_by_no(travel_params[:no_2])) : ''
+    travel_params[:no_3] ? @travels.push(Travel.find_by_no(travel_params[:no_3])) : ''
+
+    passengerIds = []
+
+    @travels.each do |travel|
+      8.times do |count|
+        params["passenger_#{count+1}"] ? passenger = travel.passengers.build(passenger_params(count+1)) : break
+        passenger.save
+        passengerIds.push(passenger.id)
+      end
+    end
+
+    redirect_to passengers_path(travel: params[:travel], ids: passengerIds)
   end
-  def show
-    @passenger = Passenger.find(params[:id])
+
+  private
+
+  def travel_params
+    params.require(:travel).permit(:no_1, :no_2, :no_3)
+  end
+
+  def passenger_params(count)
+    params.require("passenger_#{count}").permit(:first_name, :last_name, :date_of_birth, :passport_number, :nationality, :email, :address, :tel)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :travels, only: :index
-  resources :passengers, only: [:new, :create, :show]
+  resources :passengers, only: [:index, :new, :create]
   root to: 'travels#index'
 end
